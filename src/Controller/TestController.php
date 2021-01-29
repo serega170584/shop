491a35e0a89f29837e2758e02b1232701ad0fabf
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-01-29 14:51:32 +0300

    add page query

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,8 +12,10 @@ class TestController extends AbstractController
     /**
      * @Route("/test", name="test")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        echo $request->query->get('page');
+        die('asd');
         return $this->render('test/index.html.twig', [
             'controller_name' => 'TestController',
         ]);
