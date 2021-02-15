6e47dc5d385a39027a07cc81df75a98004d8e6a1
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-02-15 12:35:43 +0300

    uploads fix

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -158,4 +158,14 @@ class Product
     {
         $this->updatedAt = new \DateTime();
     }
+
+    /**
+     * @ORM\PrePersist
+     * @ORM\PreUpdate
+     */
+    public function setImageVal()
+    {
+        var_dump($this->image);
+        die('asd');
+    }
 }
