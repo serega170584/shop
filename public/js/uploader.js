12722540d843a3e00d98dd89eeb64967b2050716
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-02-16 07:58:23 +0300

    uploads fix

--- public/js/uploader.js
+++ public/js/uploader.js
@@ -1,3 +1,5 @@
 $(function () {
-    alert('123');
+    let filename;
+    filename = $('#product_image_file').closest('.custom-file').find('.custom-file-label').html();
+    console.log(filename);
 });
