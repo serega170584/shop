3de7517de2af0dd3aec88cf91c749136794d28cd
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-02-16 08:18:13 +0300

    uploads fix

--- public/js/uploader.js
+++ public/js/uploader.js
@@ -1,7 +1,11 @@
 $(function () {
-    let filename, customFileContainer;
+    let filename, customFileContainer, fileUploadContainer;
     customFileContainer = $('#product_image_file').closest('.custom-file');
     filename = customFileContainer.find('.custom-file-label').html();
     fileUploadContainer = $('#product_image_file').closest('.easyadmin-fileupload');
     fileUploadContainer.append('<img src="/uploads/files/' + filename + '" />');
+
+    $('.easyadmin-fileupload-delete-btn').click(function () {
+        console.log(fileUploadContainer.html());
+    });
 });
