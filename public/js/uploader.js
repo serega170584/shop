829e4695b32612be61f76e2d5ddd558700f02583
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-02-16 08:03:26 +0300

    uploads fix

--- public/js/uploader.js
+++ public/js/uploader.js
@@ -2,5 +2,6 @@ $(function () {
     let filename, customFileContainer;
     customFileContainer = $('#product_image_file').closest('.custom-file');
     filename = customFileContainer.find('.custom-file-label').html();
+    customFileContainer.append('<img src="/uploads/files/' + filename + '" />');
     console.log(filename);
 });
