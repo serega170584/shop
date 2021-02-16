954d0488ef828a4ceb6f1cfa556b833af4bdef9c
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-02-16 14:54:59 +0300

    uploads fix

--- public/js/uploader.js
+++ public/js/uploader.js
@@ -1,35 +1,36 @@
-alert('8888');
-let filename, customFileContainer, fileUploadContainer;
-customFileContainer = $('#product_image_file').closest('.custom-file');
-filename = customFileContainer.find('.custom-file-label').html();
-if (filename != '') {
-    filename = '/uploads/files/' + filename;
-}
-fileUploadContainer = $('#product_image_file').closest('.easyadmin-fileupload');
-fileUploadContainer.append('<img id="image-file" src="' + filename + '" />');
-
-$('.easyadmin-fileupload-delete-btn').click(function () {
-    fileUploadContainer.find('img').remove();
-});
-
-$('#product_image_file').change(function (e) {
-    openFile(e)
-})
-
-function openFile(file) {
-    let reader = new FileReader();
-    let input = file.target;
-    file = input.files[0];
-    if (file.size < 2000) {
-        reader.onload = function () {
-            let dataURL = reader.result;
-            let imageFile = $('#image-file');
-            imageFile.attr('src', dataURL);
-        }
-        reader.readAsDataURL(file);
-    } else {
-        console.log(customFileContainer.find('.custom-file-label').html());
-        customFileContainer.find('.custom-file-label').html('');
-        alert('File size 2kB is exceeded!');
+$(function () {
+    let filename, customFileContainer, fileUploadContainer;
+    customFileContainer = $('#product_image_file').closest('.custom-file');
+    filename = customFileContainer.find('.custom-file-label').html();
+    if (filename != '') {
+        filename = '/uploads/files/' + filename;
     }
-}
+    fileUploadContainer = $('#product_image_file').closest('.easyadmin-fileupload');
+    fileUploadContainer.append('<img id="image-file" src="' + filename + '" />');
+
+    $('.easyadmin-fileupload-delete-btn').click(function () {
+        fileUploadContainer.find('img').remove();
+    });
+
+    $('#product_image_file').change(function (e) {
+        openFile(e)
+    })
+
+    function openFile(file) {
+        let reader = new FileReader();
+        let input = file.target;
+        file = input.files[0];
+        if (file.size < 2000) {
+            reader.onload = function () {
+                let dataURL = reader.result;
+                let imageFile = $('#image-file');
+                imageFile.attr('src', dataURL);
+            }
+            reader.readAsDataURL(file);
+        } else {
+            console.log(customFileContainer.find('.custom-file-label').html());
+            customFileContainer.find('.custom-file-label').html('');
+            alert('File size 2kB is exceeded!');
+        }
+    }
+});
