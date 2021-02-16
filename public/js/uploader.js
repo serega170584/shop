b0ec2918d31b3903e4b309aa5a44b2a0c9349acd
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-02-16 08:41:15 +0300

    uploads fix

--- public/js/uploader.js
+++ public/js/uploader.js
@@ -3,13 +3,25 @@ $(function () {
     customFileContainer = $('#product_image_file').closest('.custom-file');
     filename = customFileContainer.find('.custom-file-label').html();
     fileUploadContainer = $('#product_image_file').closest('.easyadmin-fileupload');
-    fileUploadContainer.append('<img src="/uploads/files/' + filename + '" />');
+    fileUploadContainer.append('<img id="image-file" src="/uploads/files/' + filename + '" />');
 
     $('.easyadmin-fileupload-delete-btn').click(function () {
         fileUploadContainer.find('img').remove();
     });
 
-    $('#product_image_file').change(function(){
-        console.log($(this).val());
+    $('#product_image_file').change(function (e) {
+        openFile(e)
     })
+
+    function openFile(file) {
+        let input = file.target;
+        console.log(input);
+        let reader = new FileReader();
+        reader.onload = function () {
+            let dataURL = reader.result;
+            let imageFile = $('#image-file');
+            imageFile.attr('src', dataURL);
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
 });
