b8e8a4b0fba9ad49ead95cbe2529790cdb1deb94
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-02-17 13:57:58 +0300

    uploads fix

--- public/js/uploader.js
+++ public/js/uploader.js
@@ -6,6 +6,7 @@ $.fn.customHtml = function () {
 }
 $.fn.html = function () {
     let result = originalHtmlClassMethod.apply(this, arguments);
+    console.log(arguments);
     if (arguments.length > 0) {
         $(this).trigger('customChange');
     }
