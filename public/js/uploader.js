a52170cbfaaac23964074b72cf458f806ec367ed
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-02-19 09:23:51 +0300

    uploads fix

--- public/js/uploader.js
+++ public/js/uploader.js
@@ -4,7 +4,7 @@ $(function () {
     $('.input-group-append').hide();
     let inputGroupAppendHtml = '';
     inputGroupAppendHtml += '<div class="custom-input-group-append">';
-    inputGroupAppendHtml += '<span class="input-group-text" id="image-size" style=""></span>';
+    inputGroupAppendHtml += '<span class="input-group-text" id="image-size" style="">asdasdasd</span>';
     inputGroupAppendHtml += '<label class="btn easyadmin-fileupload-delete-btn" style="" for="product_image_delete">';
     inputGroupAppendHtml += '<i class="fa fa-trash-o"></i>';
     inputGroupAppendHtml += '</label>';
