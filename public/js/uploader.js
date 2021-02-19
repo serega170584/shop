3de2433666af37c744c532f9b65af2f6c06b85c0
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-02-19 08:48:01 +0300

    uploads fix

--- public/js/uploader.js
+++ public/js/uploader.js
@@ -1,6 +1,18 @@
 $(function () {
     $('.custom-file-label').hide();
     $('#product_image_file').closest('.custom-file').append('<label for="product_image_file" lang="en" class="user-custom-file-label"></label>');
+    $('.input-group-append').hide();
+    let inputGroupAppendHtml = '';
+    inputGroupAppendHtml += '<div class="input-group-append">';
+    inputGroupAppendHtml += '<span class="input-group-text" style="">153K</span>';
+    inputGroupAppendHtml += '<label class="btn easyadmin-fileupload-delete-btn" style="" for="product_image_delete">';
+    inputGroupAppendHtml += '<i class="fa fa-trash-o"></i>';
+    inputGroupAppendHtml += '</label>';
+    inputGroupAppendHtml += '<label class="btn" for="product_image_file">';
+    inputGroupAppendHtml += '<i class="fa fa-folder-open-o"></i>';
+    inputGroupAppendHtml += '</label>';
+    inputGroupAppendHtml += '</div>';
+    $('#product_image_file').closest('.input-group').append(inputGroupAppendHtml);
 
     let filename, customFileContainer, fileUploadContainer;
     customFileContainer = $('#product_image_file').closest('.custom-file');
