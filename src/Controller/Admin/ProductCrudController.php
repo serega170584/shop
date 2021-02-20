fea2ed5c59b23c3e5ef62cf544d986d5aa10c88d
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-02-20 20:18:21 +0300

    add admin directory

--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Product;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 
 class ProductCrudController extends AbstractCrudController
 {
@@ -12,6 +13,12 @@ class ProductCrudController extends AbstractCrudController
         return Product::class;
     }
 
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Product')
+            ->setEntityLabelInPlural('Products');
+    }
     /*
     public function configureFields(string $pageName): iterable
     {
