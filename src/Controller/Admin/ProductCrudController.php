857b4a098e059cfb8671ee61630ee24de57eae75
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-02-24 14:24:49 +0300

    product fields fix

--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\Product;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 
 class ProductCrudController extends AbstractCrudController
 {
@@ -24,7 +25,7 @@ class ProductCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-//            IdField::new('id'),
+            IdField::new('id'),
 //            TextField::new('title'),
 //            TextEditorField::new('description'),
         ];
