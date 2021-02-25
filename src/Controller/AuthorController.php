e38dc3a7c27e7998f04be33db910ca8573b0134b
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-02-25 18:23:13 +0300

    product fields fix

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -6,6 +6,7 @@ use App\Entity\Author;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class AuthorController extends AbstractController
 {
