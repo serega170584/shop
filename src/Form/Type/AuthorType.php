103d5ab215202b144f2144f959dddcc6517d3cd7
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-02-25 19:30:08 +0300

    product fields fix

--- src/Form/Type/AuthorType.php
+++ src/Form/Type/AuthorType.php
@@ -5,6 +5,7 @@ namespace App\Form\Type;
 use App\Entity\Author;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +14,9 @@ class AuthorType extends AbstractType
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('bioFile', FileType::class);
+        $builder
+            ->add('bioFile', FileType::class)
+            ->add('save', SubmitType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
