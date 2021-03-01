b7afbdeffcd72c552f8e72de5727beb72df8168f
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-03-01 15:34:00 +0300

    files uploader fix

--- public/js/uploader.js
+++ public/js/uploader.js
@@ -1,6 +1,6 @@
 $(function () {
     let customFile = $('#Product_image_file').closest('.custom-file');
-    // $('#Product_image_file').remove();
+    $('#Product_image_file').remove();
     customFile.append('<input type="text" ' +
         'id="Product_image_file" name="Product[image][file]" required="required"' +
         ' align="center" placeholder="" title="" data-files-label="files" class="custom-file-input">');
