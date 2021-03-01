a9b5471aaad0d29af711d4bf26700466bc72d377
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-03-01 08:30:00 +0300

    author delete

--- migrations/Version20210301052457.php
+++ migrations/Version20210301052457.php
@@ -12,20 +12,18 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20210301052457 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return '';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-
+        $this->addSql('DROP TABLE author;');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-
+        $this->addSql('CREATE TABLE author (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');        // this down() migration is auto-generated, please modify it to your needs
     }
 }
