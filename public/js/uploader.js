c13cec18a84db2d1616db090cb135e941bde8978
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-03-01 16:33:14 +0300

    files uploader fix

--- public/js/uploader.js
+++ public/js/uploader.js
@@ -4,11 +4,13 @@ $(function () {
     let inputGroupAppend = inputGroup.find('.input-group-append');
     // inputGroup.closest('.ea-fileupload').find('.form-check').parent().remove();
     inputGroupAppend.find('.input-group-text').remove();
-    // inputGroupAppend.find('.ea-fileupload-delete-btn').remove();
-    //     inputGroupAppend.find('.btn').remove();
-    // inputGroupAppend.append('<label class="btn">\n' +
-    //     '                    <i class="fa fa-folder-open-o"></i>\n' +
-    //     '                </label>');
+    inputGroupAppend.find('.btn').remove();
+    inputGroupAppend.append('<label class="btn ea-fileupload-delete-btn">\n' +
+        '                    <i class="fa fa-folder-open-o"></i>\n' +
+        '                </label>');
+    inputGroupAppend.append('<label class="btn">\n' +
+        '                    <i class="fa fa-folder-open-o"></i>\n' +
+        '                </label>');
     // $('#Product_image_file').remove();
     // customFile.append('<input type="text" ' +
     //     'id="Product_image" name="Product[image]" required="required"' +
