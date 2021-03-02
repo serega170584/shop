0c7b48e146d68315d7f20ef9030aa57db2a13fd7
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-03-02 08:17:09 +0300

    files uploader fix

--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -45,7 +45,7 @@ class ProductCrudController extends AbstractCrudController
     }
 
     /**
-     * @Route("/product/upload", name=productUpload")
+     * @Route("/product/upload", name=upload")
      */
     public function upload(Request $request): Response
     {
