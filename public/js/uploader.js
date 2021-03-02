d4a58c216f31960f8db55cb2638b8f11583eeb8d
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-03-02 08:18:48 +0300

    files uploader fix

--- public/js/uploader.js
+++ public/js/uploader.js
@@ -19,7 +19,7 @@ $(function () {
     customFile.append('<input type="text" ' +
         'id="Product_image" name="Product[image]" required="required"' +
         ' align="center" placeholder="" title="" data-files-label="files" class="custom-file-input">');
-    $('.ea-fileupload').append('<iframe src="/" style="height:300px; display: none" name="win" id="upload_iframe"></iframe>');
+    $('.ea-fileupload').append('<iframe src="/product/upload" style="height:300px; display: none" name="win" id="upload_iframe"></iframe>');
 
     $('#product_upload').click(function(){
        $('#upload_iframe').show();
