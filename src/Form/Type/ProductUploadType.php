bc00ff4efd188aedbdac1a59954a77abc7a8aa3d
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-03-02 13:39:01 +0300

    files uploader fix

--- src/Form/Type/ProductUploadType.php
+++ src/Form/Type/ProductUploadType.php
@@ -5,11 +5,12 @@ namespace App\Form\Type;
 
 
 use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ProductUploadType
+class ProductUploadType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
