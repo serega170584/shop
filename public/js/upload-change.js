31a481b85a179aa4aabb54f2221f90688dbf4af2
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-03-03 13:28:50 +0300

    add checkobox to upload form

--- public/js/upload-change.js
+++ public/js/upload-change.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     function handleFiles() {
         const form = document.forms.product_upload;
-        console.log(form.elements['product_upload[isUploaded]']);
+        console.log(form.elements['product_upload[isUploaded]'].value);
         // form.submit();
     }
 });
