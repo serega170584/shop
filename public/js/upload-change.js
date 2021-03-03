3566d6f9957af7a15c3e52e37d91f5f71921dc57
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-03-03 14:27:49 +0300

    add checkobox to upload form

--- public/js/upload-change.js
+++ public/js/upload-change.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const imageFile = document.getElementById('product_upload_imageFile');
     imageFile.addEventListener('change', handleFiles, false);
-
+    win.postMessage("message", "*");
     function handleFiles() {
         const form = document.forms.product_upload;
         form.elements['product_upload[isUploaded]'].value = '1';
