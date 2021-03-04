a648e5dab0900de5ea4901e873de3c77018601a9
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-03-04 12:59:57 +0300

    upload file fix

--- public/js/upload-change.js
+++ public/js/upload-change.js
@@ -7,7 +7,6 @@ document.addEventListener("DOMContentLoaded", () => {
             alert('Size 1MB is exceeded!')
         } else {
             const form = document.forms.product_upload;
-            form.elements['product_upload[isUploaded]'].value = '1';
             form.submit();
         }
     }
