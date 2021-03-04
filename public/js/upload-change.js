848a159ae41031fd919ba26b399db87bd802b1e2
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-03-04 08:10:43 +0300

    upload file fix

--- public/js/upload-change.js
+++ public/js/upload-change.js
@@ -3,8 +3,12 @@ document.addEventListener("DOMContentLoaded", () => {
     imageFile.addEventListener('change', handleFiles, false);
 
     function handleFiles() {
+        let reader = new FileReader();
+        reader.readAsText(document.getElementById('product_upload_imageFile').files[0]);
+        reader.onload = () => console.log(reader.result);
+        reader.onerror = () => console.log(reader.error);
         const form = document.forms.product_upload;
-        form.elements['product_upload[isUploaded]'].value = '1';
-        form.submit();
+        // form.elements['product_upload[isUploaded]'].value = '1';
+        // form.submit();
     }
 });
