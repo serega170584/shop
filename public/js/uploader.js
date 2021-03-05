8dae3599de09f18da4c18c0049ffa849750bb3b4
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-03-05 14:21:13 +0300

    upload file fix

--- public/js/uploader.js
+++ public/js/uploader.js
@@ -9,7 +9,7 @@ $(function () {
         $('#Product_image').closest('.form-widget').html(`<div class="ea-fileupload">\n` +
             `        <div class="input-group">\n` +
             `                                                            <div class="custom-file">\n` +
-            `    <label for="Product_image" lang="en" class="custom-file-label" id="file_label">${productImage}</label><input type="text" id="Product_image" name="Product[image]" value="${productImage}" required="required" align="center" placeholder="" title="" data-files-label="files" class="custom-file-input"></div>\n` +
+            `    <label for="Product_image" lang="en" class="custom-file-label" id="file_label">${productImage}</label><input type="text" id="Product_image" name="Product[image]" value="${productImage}" align="center" placeholder="" title="" data-files-label="files" class="custom-file-input"></div>\n` +
             `            <div class="input-group-append">\n` +
             `                \n` +
             `                                    \n` +
