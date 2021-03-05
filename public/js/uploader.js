c7e0c9ad583c6a758a7474abade243ebdd5354b9
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-03-05 10:09:33 +0300

    upload file fix

--- public/js/uploader.js
+++ public/js/uploader.js
@@ -1,12 +1,13 @@
 $(function () {
         let display = 'none';
-        if ($('#Product_image').val()) {
+        let productImage = $('#Product_image').val();
+        if (productImage) {
             display = 'block';
         }
         $('#Product_image').closest('.form-widget').html(`<div class="ea-fileupload">\n` +
             `        <div class="input-group">\n` +
             `                                                            <div class="custom-file">\n` +
-            `    <label for="Product_image" lang="en" class="custom-file-label" id="file_label"></label><input type="text" id="Product_image" name="Product[image]" required="required" align="center" placeholder="" title="" data-files-label="files" class="custom-file-input"></div>\n` +
+            `    <label for="Product_image" lang="en" class="custom-file-label" id="file_label">${productImage}</label><input type="text" id="Product_image" name="Product[image]" value="${productImage}" required="required" align="center" placeholder="" title="" data-files-label="files" class="custom-file-input"></div>\n` +
             `            <div class="input-group-append">\n` +
             `                \n` +
             `                                    \n` +
@@ -18,10 +19,10 @@ $(function () {
             `                </label></div>\n` +
             `        </div>\n` +
             `                            \n` +
-            `            <iframe src="/product/upload" style="height:300px; display: none" name="win" id="upload_iframe"></iframe></div>`)
+            `            <iframe src="/product/upload" style="height:300px; display: none" name="win" id="upload_iframe"></iframe></div>`);
 
-        $('#product_upload').click(function () {
-            $('#upload_iframe').show();
-        });
+        // $('#product_upload').click(function () {
+        //     $('#upload_iframe').show();
+        // });
     }
 );
