e8b71770974193d05f79f884f5fd0b7c0b2302ce
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-03-05 09:52:05 +0300

    upload file fix

--- public/js/uploader.js
+++ public/js/uploader.js
@@ -1,20 +1,20 @@
 $(function () {
-    $('#Product_image').closest('.form-widget').html('<div class="ea-fileupload">\n' +
-        '        <div class="input-group">\n' +
-        '                                                            <div class="custom-file">\n' +
-        '    <label for="Product_image" lang="en" class="custom-file-label" id="file_label"></label><input type="text" id="Product_image" name="Product[image]" required="required" align="center" placeholder="" title="" data-files-label="files" class="custom-file-input"></div>\n' +
-        '            <div class="input-group-append">\n' +
-        '                \n' +
-        '                                    \n' +
-        '                                \n' +
-        '            <label class="btn" style="display: none">\n' +
-        '                    <i class="fa fa-trash-o"></i>\n' +
-        '                </label><label id="product_upload" class="btn">\n' +
-        '                    <i class="fa fa-folder-open-o"></i>\n' +
-        '                </label></div>\n' +
-        '        </div>\n' +
-        '                            \n' +
-        '            <iframe src="/product/upload" style="height:300px; display: none" name="win" id="upload_iframe"></iframe></div>')
+    $('#Product_image').closest('.form-widget').html(`<div class="ea-fileupload">\n` +
+        `        <div class="input-group">\n` +
+        `                                                            <div class="custom-file">\n` +
+        `    <label for="Product_image" lang="en" class="custom-file-label" id="file_label"></label><input type="text" id="Product_image" name="Product[image]" required="required" align="center" placeholder="" title="" data-files-label="files" class="custom-file-input"></div>\n` +
+        `            <div class="input-group-append">\n` +
+        `                \n` +
+        `                                    \n` +
+        `                                \n` +
+        `            <label class="btn" style="display: none">\n` +
+        `                    <i class="fa fa-trash-o"></i>\n` +
+        `                </label><label id="product_upload" class="btn">\n` +
+        `                    <i class="fa fa-folder-open-o"></i>\n` +
+        `                </label></div>\n` +
+        `        </div>\n` +
+        `                            \n` +
+        `            <iframe src="/product/upload" style="height:300px; display: none" name="win" id="upload_iframe"></iframe></div>`)
 
     $('#product_upload').click(function () {
         $('#upload_iframe').show();
