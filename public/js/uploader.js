ed4a6b3256f22729606b1dd67fbe2c324749420d
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-03-09 13:39:27 +0300

    upload file fix

--- public/js/uploader.js
+++ public/js/uploader.js
@@ -8,13 +8,14 @@ $(function () {
         }
         let invalidFeedback = '';
         if ((invalidFeedback = $('#Product_image').closest('.form-widget').find('.invalid-feedback').html()) !== undefined) {
-            invalidFeedback =
+            invalidFeedback = `<span class="invalid-feedback d-block">${invalidFeedback}</span>`;
         }
         console.log($('#Product_image').closest('.form-widget').find('.invalid-feedback').html());
         $('#Product_image').closest('.form-widget').html(`<div class="ea-fileupload">\n` +
             `        <div class="input-group">\n` +
             `                                                            <div class="custom-file">\n` +
             `    <label for="Product_image" lang="en" class="custom-file-label" id="file_label">${productImage}</label><input type="text" id="Product_image" name="Product[image]" value="${productImage}" align="center" placeholder="" title="" data-files-label="files" class="custom-file-input"></div>\n` +
+            invalidFeedback +
             `            <div class="input-group-append">\n` +
             `                \n` +
             `                                    \n` +
