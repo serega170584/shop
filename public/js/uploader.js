681ef4998b69e851c9df1078e322b888b11ac360
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-03-10 07:51:17 +0300

    upload file fix

--- public/js/uploader.js
+++ public/js/uploader.js
@@ -11,44 +11,45 @@ $(function () {
             invalidFeedback = $('#Product_image').closest('.form-widget').find('.invalid-feedback').html();
             invalidFeedback = `<span class="invalid-feedback d-block">${invalidFeedback}</span>`;
         }
-        let formWidget = $('#Product_image').closest('.form-widget');
-        $('#Product_image').remove();
-        formWidget.html(`<div class="ea-fileupload">\n` +
-            `        <div class="input-group">\n` +
-            `                                                            <div class="custom-file">\n` +
-            `    <label for="Product_image" lang="en" class="custom-file-label" id="file_label">${productImage}</label><input type="text" id="Product_image" name="Product[image]" value="${productImage}" align="center" placeholder="" title="" data-files-label="files" class="custom-file-input"></div>\n` +
-            `            <div class="input-group-append">\n` +
-            `                \n` +
-            `                                    \n` +
-            `                                \n` +
-            `            <label class="btn" id="image_trash" style="display: ${display}">\n` +
-            `                    <i class="fa fa-trash-o"></i>\n` +
-            `                </label><label id="product_upload" class="btn">\n` +
-            `                    <i class="fa fa-folder-open-o"></i>\n` +
-            `                </label></div>\n` +
-            `        </div>\n` +
-            invalidFeedback +
-            img +
-            `                            \n` +
-            `            <iframe src="/product/upload" style="height:300px; display: none" name="win" id="upload_iframe"></iframe></div>`);
-
-        $('#product_upload').click(function () {
-            $('#upload_iframe').show();
-        });
-
-        $('#image_trash').click(function () {
-            $(this).hide();
-            $('#product_image').remove();
-            $('#Product_image').val('');
-            $('#file_label').html('');
-            alert($('#Product_image').closest('.form-widget').html());
-        });
-
-        $('#new-Product-form').submit(function (e) {
-            // if (!$('#Product_image').val().trim()) {
-            //     e.preventDefault();
-            //     alert('Upload image please!');
-            // }
-        });
+        $('#Product_image').val('123');
+        // let formWidget = $('#Product_image').closest('.form-widget');
+        // $('#Product_image').remove();
+        // formWidget.html(`<div class="ea-fileupload">\n` +
+        //     `        <div class="input-group">\n` +
+        //     `                                                            <div class="custom-file">\n` +
+        //     `    <label for="Product_image" lang="en" class="custom-file-label" id="file_label">${productImage}</label><input type="text" id="Product_image" name="Product[image]" value="${productImage}" align="center" placeholder="" title="" data-files-label="files" class="custom-file-input"></div>\n` +
+        //     `            <div class="input-group-append">\n` +
+        //     `                \n` +
+        //     `                                    \n` +
+        //     `                                \n` +
+        //     `            <label class="btn" id="image_trash" style="display: ${display}">\n` +
+        //     `                    <i class="fa fa-trash-o"></i>\n` +
+        //     `                </label><label id="product_upload" class="btn">\n` +
+        //     `                    <i class="fa fa-folder-open-o"></i>\n` +
+        //     `                </label></div>\n` +
+        //     `        </div>\n` +
+        //     invalidFeedback +
+        //     img +
+        //     `                            \n` +
+        //     `            <iframe src="/product/upload" style="height:300px; display: none" name="win" id="upload_iframe"></iframe></div>`);
+        //
+        // $('#product_upload').click(function () {
+        //     $('#upload_iframe').show();
+        // });
+        //
+        // $('#image_trash').click(function () {
+        //     $(this).hide();
+        //     $('#product_image').remove();
+        //     $('#Product_image').val('');
+        //     $('#file_label').html('');
+        //     alert($('#Product_image').closest('.form-widget').html());
+        // });
+        //
+        // $('#new-Product-form').submit(function (e) {
+        //     // if (!$('#Product_image').val().trim()) {
+        //     //     e.preventDefault();
+        //     //     alert('Upload image please!');
+        //     // }
+        // });
     }
 );
