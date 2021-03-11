f61b6433363e6e71823103edde9e906b9ffedd20
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-03-11 12:55:07 +0300

    upload file fix

--- public/js/upload-change.js
+++ public/js/upload-change.js
@@ -6,6 +6,8 @@ document.addEventListener("DOMContentLoaded", () => {
         if (document.getElementById('product_upload_imageFile').files[0].size > 1) {
             alert('Size 1MB is exceeded!');
             this.closest('#product_upload').innerHTML = '<div><label for="product_upload_imageFile" class="required">Image</label><input type="file" id="product_upload_imageFile" name="product_upload[imageFile]" required="required"></div>';
+            const imageFile = document.getElementById('product_upload_imageFile');
+            imageFile.addEventListener('change', handleFiles, false);
         } else {
             const form = document.forms.product_upload;
             form.submit();
