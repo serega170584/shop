78121ac3af5423ea8250e06bba8173fe1b04c571
Sergey Krivobokov <serega170584@gmail.com>
2021-03-12 09:19:49 +0300

    Add category migration

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -39,6 +39,11 @@ class Category
      */
     private $products;
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $image;
+
     public function __construct()
     {
         $this->products = new ArrayCollection();
@@ -111,6 +116,18 @@ class Category
                 $product->setCategory(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
 
         return $this;
     }
