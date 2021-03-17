a9152adfd49ba0bff4fea3a14cff5310d19d584b
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-03-17 13:38:15 +0300

    add category select

--- src/Controller/Admin/CategoryCrudController.php
+++ src/Controller/Admin/CategoryCrudController.php
@@ -6,11 +6,14 @@ namespace App\Controller\Admin;
 
 use App\Entity\Category;
 use App\Form\Type\CategoryUploadType;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Option\EA;
 use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
@@ -84,8 +87,13 @@ class CategoryCrudController extends AbstractCrudController
 
     public function detail(AdminContext $context)
     {
-        var_dump($context->getEntity()->getInstance()->getId());
-        die('asd');
-        $this->redirect($this->generateUrl());
+        $this->redirect($this->get(AdminUrlGenerator::class)
+            ->setController(self::getEntityFqcn())
+            ->setAction(Action::EDIT)
+            ->setEntityId($context->getEntity()->getPrimaryKeyValue())
+            ->unset(EA::MENU_INDEX)
+            ->unset(EA::SUBMENU_INDEX)
+            ->includeReferrer()
+            ->generateUrl());
     }
 }
