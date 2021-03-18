c672081f7b05e7a2ad354910c59329da9b914dc5
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-03-18 15:10:55 +0300

    add category select

--- src/Controller/Admin/CategoryCrudController.php
+++ src/Controller/Admin/CategoryCrudController.php
@@ -42,6 +42,7 @@ class CategoryCrudController extends AbstractCrudController
         $delete = Action::new(Action::DELETE)->displayIf(static function ($entity) {
             return 1 == 0;
         });
+        $actions->remove(Crud::PAGE_INDEX, $delete);
         return $actions->add(Crud::PAGE_INDEX, $delete);
     }
 
