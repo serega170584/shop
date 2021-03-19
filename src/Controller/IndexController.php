05affe18877bfc5b1590692109b1dd73605fb0ed
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-03-19 13:02:21 +0300

    category delete action drop

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +14,12 @@ class IndexController extends AbstractController
      */
     public function index(): Response
     {
+        $categories = $this->getDoctrine()->getManager()
+            ->getRepository(Category::class);
+        var_dump(get_class($categories));
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
+            'category'
         ]);
     }
 }
