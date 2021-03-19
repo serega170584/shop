ffa83531f7b43fd98f25b0e4a0ea445fa488d827
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-03-19 09:09:15 +0300

    category delete action drop

--- src/Controller/Admin/CategoryCrudController.php
+++ src/Controller/Admin/CategoryCrudController.php
@@ -42,7 +42,7 @@ class CategoryCrudController extends AbstractCrudController
         $actions->remove(Crud::PAGE_INDEX, Action::DELETE);
         $delete = Action::new(Action::DELETE, false, 'fas fa-file-invoice')->displayIf(static function ($entity) {
             return 1 == 1;
-        })->linkToRoute('new', function (Category $cat) {
+        })->linkToRoute('new', function (Category $cat):array {
             return $cat->getId();
         });
         return $actions->add(Crud::PAGE_INDEX, $delete);
