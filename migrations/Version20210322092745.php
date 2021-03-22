0448be5045201f9064ad1242019ed3c0c445cc7a
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-03-22 12:31:10 +0300

    add category title unique contraint

--- migrations/Version20210322092745.php
+++ migrations/Version20210322092745.php
@@ -20,12 +20,12 @@ final class Version20210322092745 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-
+        $this->addSql('ALTER TABLE category ADD UNIQUE (title)');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-
+        $this->addSql('ALTER TABLE category DROP CONSTRAINT title');
     }
 }
