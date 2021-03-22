27b0e1297180fb8b0396f2cbe84fb915def77e30
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-03-22 13:57:53 +0300

    add category title unique contraint

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -182,6 +182,7 @@ class Category
     public function computeSlug(SluggerInterface $slugger)
     {
         $rule=new ArrayCollection();
+        setlocale(LC_ALL, 'ru_RU.UTF8');
         var_dump(iconv('UTF-8', 'ASCII//TRANSLIT', 'фывфывфывфывфывф'));
         var_dump(\strlen('ыфвфывфывфыв'));
         die('asd');
