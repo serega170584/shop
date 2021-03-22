fcb588d8217993b78ca38f739a9e65bc9204779a
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-03-22 12:22:40 +0300

    add category slug listener

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -8,10 +8,12 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=CategoryRepository::class)
  * @ORM\HasLifecycleCallbacks()
+ * @UniqueEntity("title")
  */
 class Category
 {
