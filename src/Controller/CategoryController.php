b478ee7d37b42626ffd65169f8e24f0b8fd549dc
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-03-22 15:03:58 +0300

    category url fix

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -4,12 +4,28 @@
 namespace App\Controller;
 
 
+use App\Entity\Category;
+use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
 
 class CategoryController
 {
-    public function show(string $slug):Response
+    /**
+     * @Route("/category/{slug}", name="category")
+     * @param Environment $twig
+     * @param Category $category
+     * @param ProductRepository $productRepository
+     * @return Response
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function show(Environment $twig, Category $category, ProductRepository $productRepository): Response
     {
-
+        return new Response($twig->render('category/show.html.twig', [
+            'category' => $category,
+            'comments' => $productRepository->findBy(['category' => $category], ['createdAt' => 'DESC']),
+        ]));
     }
 }
