af056604e3f2c10e379fe124dc4b6333ca09c2c5
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-03-22 12:13:32 +0300

    add category slug listener

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -6,6 +6,7 @@ use App\Repository\CategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
@@ -171,5 +172,12 @@ class Category
         $this->slug = $slug;
 
         return $this;
+    }
+
+    public function computeSlug(SluggerInterface $slugger)
+    {
+        if (!$this->slug || '-' === $this->slug) {
+            $this->slug = (string)$slugger->slug((string)$this)->lower();
+        }
     }
 }
---
+++ src/EntityListerner/CategoryEntityListener.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace App\EntityListerner;
+
+
+use App\Entity\Category;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class CategoryEntityListener
+{
+    private $slugger;
+
+    public function __construct(SluggerInterface $slugger)
+    {
+        $this->slugger = $slugger;
+    }
+
+    public function prePersist(Category $category, LifecycleEventArgs $event)
+    {
+        $category->computeSlug($this->slugger);
+    }
+
+    public function preUpdate(Category $category, LifecycleEventArgs $event)
+    {
+        $category->computeSlug($this->slugger);
+    }
+}
