18d1e32bf3d349d8325b0e874d41f9f9ee53820d
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-03-22 08:57:54 +0300

    product is popular modify

--- migrations/Version20210319130458.php
+++ migrations/Version20210319130458.php
@@ -20,12 +20,12 @@ final class Version20210319130458 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-
+        $this->addSql('ALTER TABLE product MODIFY is_popular TINYINT(1) DEFAULT FALSE');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-
+        $this->addSql('ALTER TABLE product MODIFY is_popular TINYINT(1) DEFAULT NULL');
     }
 }
