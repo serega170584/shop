422264df4a06976fd168bfde390237449545c54c
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-03-22 13:32:52 +0300

    add category title unique contraint

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -181,7 +181,7 @@ class Category
 
     public function computeSlug(SluggerInterface $slugger)
     {
-        var_dump(\Transliterator::create('Russian-Latin'));
+        var_dump($slugger);
         die('asd');
         if (!$this->slug || '-' === $this->slug) {
             $this->slug = (string)$slugger->slug((string)$this, '-', 'ru')->lower();
