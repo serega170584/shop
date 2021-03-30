423249e6132890ce802a7c998b60a57aec4773b9
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-03-30 17:55:21 +0300

    page title fix

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -55,7 +55,7 @@ class IndexController extends AbstractController
     }
 
     /**
-     * @Route("/popular", name="popular-products-list")
+     * @Route("/popular/list", name="popular-products-list")
      */
     public function popularList(): Response
     {
