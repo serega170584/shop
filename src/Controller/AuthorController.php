d7d90607894c254c2a5c7492f47c1150d74f9c0e
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-03-30 10:11:13 +0300

    add author template

---
+++ src/Controller/AuthorController.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Entity\Author;
+use App\Repository\ProductRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AuthorController
+{
+    /**
+     * @Route("/author/{slug}", name="author")
+     * @param Environment $twig
+     * @param Author $author
+     * @param ProductRepository $productRepository
+     * @return Response
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function show(Environment $twig, Author $author, ProductRepository $productRepository): Response
+    {
+        return new Response($twig->render('author/show.html.twig', [
+            'title' => $author->getTitle(),
+            'author' => $author,
+            'products' => $productRepository->findBy(['author' => $author], ['createdAt' => 'DESC']),
+        ]));
+    }
+}
