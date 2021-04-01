3fa710d013e43037dc8f5b8016e485b51b888a07
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-04-01 09:39:39 +0300

    page content fix

--- src/EventSubscriber/TwigEventSubscriber.php
+++ src/EventSubscriber/TwigEventSubscriber.php
@@ -2,14 +2,25 @@
 
 namespace App\EventSubscriber;
 
+use App\Repository\CategoryRepository;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\ControllerEvent;
+use Twig\Environment;
 
 class TwigEventSubscriber implements EventSubscriberInterface
 {
+    private $twig;
+    private $categoryRepository;
+
+    public function __construct(Environment $twig, CategoryRepository $categoryRepository)
+    {
+        $this->twig = $twig;
+        $this->categoryRepository = $categoryRepository;
+    }
+
     public function onKernelController(ControllerEvent $event)
     {
-        // ...
+        $this->twig->addGlobal('categories', $this->categoryRepository->findAll());
     }
 
     public static function getSubscribedEvents()
