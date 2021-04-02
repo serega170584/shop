35534836a5dc2c9a234e5fc39226b59edf525bf3
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-04-02 15:15:35 +0300

    events view fix

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Event;
+use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,6 +19,19 @@ class EventController extends AbstractController
     {
         return $this->render('event/index.html.twig', [
             'event' => $event
+        ]);
+    }
+
+    /**
+     * @Route("/events", name="events")
+     * @param EventRepository $eventRepository
+     * @return Response
+     */
+    public function events(EventRepository $eventRepository): Response
+    {
+        return $this->render('event/list.html.twig', [
+            'title'=> 'События',
+            'events' => $eventRepository->findAll()
         ]);
     }
 }
