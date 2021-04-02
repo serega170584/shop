c1873b3fd6732fd7a7fe66e40066f3229d5c9c06
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-04-02 08:51:30 +0300

    product page fix

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +10,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProductController extends AbstractController
 {
     /**
-     * @Route("/product", name="product")
+     * @Route("/product/{slug}", name="product")
+     * @param Product $product
+     * @return Response
      */
-    public function index(): Response
+    public function index(Product $product): Response
     {
         return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+            'product' => $product,
         ]);
     }
 }
