830591a6f680bc04dada080b1c32778507fc9ba1
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-04-05 16:08:52 +0300

    session check

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -30,7 +30,7 @@ class IndexController extends AbstractController
     public function index(\Symfony\Component\HttpFoundation\Session\SessionInterface $request): Response
     {
         $request->start();
-        $request->get('test');
+        var_dump($request->get('test'));
         $request->set('test', 123123123);
 //        var_dump($request->read('abc'));
 //        $request->write('abc', 'dasdasdasdasdasdsad');
