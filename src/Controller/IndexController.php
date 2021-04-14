a5e7f5b528f45cef31677dc5c21954a7e12846e4
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-04-14 09:09:19 +0300

    session check

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -31,6 +31,8 @@ class IndexController extends AbstractController
      */
     public function index(Request $request): Response
     {
+        var_dump($request->getSession()->get('test1122'));
+        var_dump($request->getSession()->set('test1122', 88888888888888888888));
         var_dump($request->getSession()->getId());
         var_dump($request->getSession()->get('test1122'));
 //        $request->getSession()->set('test1122', 88888888888888888888);
