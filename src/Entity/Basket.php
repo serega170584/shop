0ac57b94471fc63d4b90ffabe5b1e3861f0a6c12
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-04-19 09:24:07 +0300

    basket add

--- src/Entity/Basket.php
+++ src/Entity/Basket.php
@@ -28,7 +28,7 @@ class Basket
     private $updatedAt;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, unique=true)
      */
     private $sessionId;
 
