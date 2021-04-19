787b49e37eb5bea9b65ce085afd5bbee1fb36b90
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-04-19 10:03:55 +0300

    order session id delete

--- src/Entity/Order.php
+++ src/Entity/Order.php
@@ -9,9 +9,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=OrderRepository::class)
- * @ORM\Table(name="`order`", uniqueConstraints={
- *      @ORM\UniqueConstraint(name="sessionCreatedAt", columns={"session_id", "created_at"})
- * })
+ * @ORM\Table(name="`order`")
  */
 class Order
 {
@@ -21,11 +19,6 @@ class Order
      * @ORM\Column(type="integer")
      */
     private $id;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $sessionId;
 
     /**
      * @ORM\Column(type="datetime")
@@ -56,18 +49,6 @@ class Order
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getSessionId(): ?string
-    {
-        return $this->sessionId;
-    }
-
-    public function setSessionId(string $sessionId): self
-    {
-        $this->sessionId = $sessionId;
-
-        return $this;
     }
 
     public function getCreatedAt(): ?\DateTimeInterface
