103c926825620f4902733a64c63f624735c1a726
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-04-19 09:40:17 +0300

    session order item delete

---
+++ src/Entity/SessionOrderItem.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SessionOrderItemRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=SessionOrderItemRepository::class)
+ * @ORM\Table(name="`session_order_item`", uniqueConstraints={
+ *      @ORM\UniqueConstraint(name="productOrder", columns={"product_id", "product_order_id"})
+ * })
+ */
+class SessionOrderItem
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Product::class, inversedBy="sessionOrderItems")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $product;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $count;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Order::class, inversedBy="sessionOrderItems")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $productOrder;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getProduct(): ?Product
+    {
+        return $this->product;
+    }
+
+    public function setProduct(?Product $product): self
+    {
+        $this->product = $product;
+
+        return $this;
+    }
+
+    public function getCount(): ?int
+    {
+        return $this->count;
+    }
+
+    public function setCount(int $count): self
+    {
+        $this->count = $count;
+
+        return $this;
+    }
+
+    public function getProductOrder(): ?Order
+    {
+        return $this->productOrder;
+    }
+
+    public function setProductOrder(?Order $productOrder): self
+    {
+        $this->productOrder = $productOrder;
+
+        return $this;
+    }
+}
