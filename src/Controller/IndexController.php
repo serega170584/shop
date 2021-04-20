79edd00fb27701f335f2085528d79305ad2141e4
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-04-20 15:14:54 +0300

    buy button check

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -30,7 +30,7 @@ class IndexController extends AbstractController
      * @param Request $request
      * @return Response
      */
-    public function index(Request $request): Response
+    public function index(Request $request, Category $cat): Response
     {
         /**
          * @var CategoryRepository $repository
