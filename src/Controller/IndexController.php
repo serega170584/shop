d0345cd6e4924269e73b8ef5c2f8c6398d517f85
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-04-22 09:45:36 +0300

    form check

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -89,7 +89,7 @@ class IndexController extends AbstractController
         $count = 0;
         $text = '';
         if ($form->isSubmitted() && $form->isValid()) {
-            echo $form->getNormData()['product_id'];
+            echo $form->getNormData();
 //            var_dump($form->get('productId')->getData());
             die('asd');
         }
