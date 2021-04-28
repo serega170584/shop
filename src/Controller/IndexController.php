565a920c0d316f106a5ab8a68c82ba22c2406b6d
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-04-28 12:42:34 +0300

    product form types delete

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -104,10 +104,10 @@ class IndexController extends AbstractController
         $form->handleRequest($request);
         $request->getSession()->start();
         $sessionId = $request->getSession()->getId();
+        die('asd');
         if (!($basket = $repository->findOneBy(['sessionId' => $sessionId]))) {
             $basket = $factory->getBasket();
         }
-        die('asd');
         if ($form->isSubmitted() && $form->isValid()) {
             var_dump($basketItem->getId());
             die('asd');
