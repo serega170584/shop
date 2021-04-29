9b6c5efdcd303f5a678d93fbda5a8c610bb7a71b
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-04-29 12:03:36 +0300

    basket items count fix

--- src/EventSubscriber/TwigEventSubscriber.php
+++ src/EventSubscriber/TwigEventSubscriber.php
@@ -3,6 +3,7 @@
 namespace App\EventSubscriber;
 
 use App\Factory\BasketFactory;
+use App\Repository\BasketRepository;
 use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
@@ -18,14 +19,19 @@ class TwigEventSubscriber implements EventSubscriberInterface
      * @var BasketFactory
      */
     private $basketFactory;
+    /**
+     * @var BasketRepository
+     */
+    private $basketRepository;
 
     public function __construct(Environment $twig, CategoryRepository $categoryRepository, ProductRepository $productRepository,
-                                BasketFactory $basketFactory)
+                                BasketFactory $basketFactory, BasketRepository $basketRepository)
     {
         $this->twig = $twig;
         $this->categoryRepository = $categoryRepository;
         $this->productRepository = $productRepository;
         $this->basketFactory = $basketFactory;
+        $this->basketRepository = $basketRepository;
     }
 
     public function onKernelController(ControllerEvent $event)
@@ -33,8 +39,14 @@ class TwigEventSubscriber implements EventSubscriberInterface
         $event->getRequest()->getSession()->start();
         $this->twig->addGlobal('categories', $this->categoryRepository->findAll());
         $this->twig->addGlobal('popularProducts', $this->productRepository->findPopular());
-        $this->basketFactory->getBasket()->setSessionId($event->getRequest()->getSession()->getId());
-        $this->twig->addGlobal('basket', $this->basketFactory->getBasket());
+        $id = $event->getRequest()->getSession()->getId();
+        if (!($basket = $this->basketRepository->findOneBy([
+            'sessionId' => $id
+        ]))) {
+            $basket = $this->basketFactory->getBasket();
+            $basket->setSessionId($id);
+        }
+        $this->twig->addGlobal('basket', $basket);
     }
 
     public static function getSubscribedEvents()
