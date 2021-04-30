344b2771085b873b94a3a8f65724413fb7110a8d
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-04-30 11:47:21 +0300

    basket items count fix

--- src/EventSubscriber/TwigEventSubscriber.php
+++ src/EventSubscriber/TwigEventSubscriber.php
@@ -50,6 +50,7 @@ class TwigEventSubscriber implements EventSubscriberInterface
         } else {
             $basket->setSessionId($id);
         }
+        var_dump($basket->getSessionId());
         var_dump($this->basketFactory->getBasket()->getSessionId());
         $this->twig->addGlobal('basket', $basket);
     }
