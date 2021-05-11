80da65fb9ec6ec552338e60b674b01d863fb7f79
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-05-11 10:32:19 +0300

    basket title fix

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -225,6 +225,9 @@ class IndexController extends AbstractController
      */
     public function basket(): Response
     {
-        return $this->render('frontend/basket.html.twig');
+        return $this->render('frontend/basket.html.twig',[
+            'title' => 'Корзина' .
+                '',
+        ]);
     }
 }
