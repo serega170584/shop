6217753f43c66b3822304bf22fe07ad3607d9d91
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-05-21 14:58:25 +0300

    order form add

---
+++ src/Form/OrderFormType.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace App\Form;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class OrderFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class);
+
+        $builder
+            ->add('telephone', TelType::class);
+
+        $builder
+            ->add('address', TextareaType::class);
+    }
+}
