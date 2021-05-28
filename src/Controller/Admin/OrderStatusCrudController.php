e274acd0f919792809ba386daf9b1233c9243d7c
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-05-28 15:01:44 +0300

    order status fix

--- src/Controller/Admin/OrderStatusCrudController.php
+++ src/Controller/Admin/OrderStatusCrudController.php
@@ -6,9 +6,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\OrderStatus;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
-class OrderStatusCrudController
+class OrderStatusCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
     {
