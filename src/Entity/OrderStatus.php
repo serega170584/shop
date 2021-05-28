afbdd322e64c348be8038fb602c3cd49f8e97c28
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-05-28 15:03:08 +0300

    order status fix

--- src/Entity/OrderStatus.php
+++ src/Entity/OrderStatus.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=OrderStatusRepository::class)
