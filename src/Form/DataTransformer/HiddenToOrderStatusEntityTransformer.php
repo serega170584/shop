87275afeaa226a0ea548cf7ef9be484486cb09dc
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-05-31 14:04:58 +0300

    order status fix

---
+++ src/Form/DataTransformer/HiddenToOrderStatusEntityTransformer.php
@@ -0,0 +1,64 @@
+<?php
+
+
+namespace App\Form\DataTransformer;
+
+
+use App\Entity\OrderStatus;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\Exception\TransformationFailedException;
+
+class HiddenToOrderStatusEntityTransformer implements DataTransformerInterface
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @param OrderStatus $value
+     * @return string
+     */
+    public function transform($value): string
+    {
+        if (null === $value) {
+            return '';
+        }
+
+        return $value->getId();
+    }
+
+    /**
+     * @param mixed $value
+     * @return OrderStatus|null
+     */
+    public function reverseTransform($value): ?Product
+    {
+        if (!$value) {
+            return null;
+        }
+
+        /**
+         * @var OrderStatus $orderStatus
+         */
+        $orderStatus = $this->entityManager
+            ->getRepository(OrderStatus::class)
+            ->find($value);
+
+        if (null === $orderStatus) {
+            throw new TransformationFailedException(sprintf(
+                'An order status with number "%s" does not exist!',
+                $value
+            ));
+        }
+
+        return $orderStatus;
+    }
+}
--- src/Form/OrderFormType.php
+++ src/Form/OrderFormType.php
@@ -4,16 +4,26 @@
 namespace App\Form;
 
 
+use App\Form\DataTransformer\HiddenToOrderStatusEntityTransformer;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class OrderFormType extends AbstractType
 {
+    /**
+     * @var HiddenToOrderStatusEntityTransformer
+     */
+    private $transformer;
+
+    public function __construct(HiddenToOrderStatusEntityTransformer $transformer)
+    {
+        $this->transformer = $transformer;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -28,5 +38,7 @@ class OrderFormType extends AbstractType
         $builder
             ->add('orderStatus', HiddenType::class);
 
+        $builder->get('orderStatus')
+            ->addModelTransformer($this->transformer);
     }
 }
