8542fc73f2723928124e602396ce24896cfef28a
Sergey Krivobokov <krivobokov.sv@academia-moscow.ru>
2021-07-26 12:44:33 +0300

    basket total fix

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -287,6 +287,7 @@ class IndexController extends AbstractController
             $basket->setIsActive(false);
             $entityManager->persist($basket);
             $entityManager->flush();
+            $request->getSession()->migrate();
             return $this->redirectToRoute('index');
         }
         return $this->render('basket/checkout.html.twig', [
