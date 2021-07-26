9d4211ab89c431d1b8856617786a53813fc22cea
Sergey Krivobokov <krivobokov.sv@academia-moscow.ru>
2021-07-26 10:55:08 +0300

    news cr fix

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Entity\News;
-use App\Repository\NewsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -24,14 +23,16 @@ class NewsController extends AbstractController
 
     /**
      * @Route("/newsList", name="newsList")
-     * @param NewsRepository $newsRepository
      * @return Response
      */
-    public function newsList(NewsRepository $newsRepository): Response
+    public function newsList(): Response
     {
         return $this->render('news/list.html.twig', [
             'title'=> 'Новости',
-            'newsList' => $newsRepository->findAll()
+            'newsList' => $this->getDoctrine()
+                ->getManager()
+                ->getRepository(News::class)
+                ->findAll()
         ]);
     }
 }
