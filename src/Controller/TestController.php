65cbe36ca4f19b5d34d2210323ad8485825d6cb1
Sergey Krivobokov <krivobokov.sv@academia-moscow.ru>
2021-09-04 08:23:32 +0300

    auth fix

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -6,6 +6,7 @@ use App\Repository\DigitalLineTestGroupRepository;
 use App\Repository\DigitalLineTestRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class TestController extends AbstractController
 {
