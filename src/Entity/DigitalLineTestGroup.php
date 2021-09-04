fb96b24f40e9965c32523effd1fde341b5e4fb13
Sergey Krivobokov <krivobokov.sv@academia-moscow.ru>
2021-09-04 08:32:13 +0300

    digital subgroup fix

--- src/Entity/DigitalLineTestGroup.php
+++ src/Entity/DigitalLineTestGroup.php
@@ -5,6 +5,8 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Collections\Criteria;
+use App\Repository\DigitalLineTestRepository;
+use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=DigitalLineTestGroupRepository::class)
