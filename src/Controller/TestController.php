808949a173d7d023ed9ed7e594923f1c7cfac627
Sergey Krivobokov <krivobokov.sv@academia-moscow.ru>
2021-09-05 06:36:12 +0300

    digital subgroup fix

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,12 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\DigitalLineTest;
+use App\Entity\DigitalLineTestGroup;
+use App\Entity\DigitalLineTestSubGroup;
 use App\Repository\DigitalLineTestGroupRepository;
 use App\Repository\DigitalLineTestRepository;
 use App\Repository\DigitalLineTestSubGroupRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class TestController extends AbstractController
 {
@@ -20,7 +22,18 @@ class TestController extends AbstractController
      */
     public function index(DigitalLineTestSubGroupRepository $digitalLineTestSubGroupRepository, DigitalLineTestRepository $digitalLineTestRepository, DigitalLineTestGroupRepository $digitalLineTestGroupRepository): Response
     {
-        $tests = $digitalLineTestRepository->findAll();
+        $group = new DigitalLineTestGroup();
+        $group->setName('asdasd');
+        $this->getDoctrine()->getManager()->persist($group);
+        $subGroup = new DigitalLineTestSubGroup();
+        $subGroup->setName('asdasd');
+        $subGroup->setDigitalLineTestGroup($group);
+        $this->getDoctrine()->getManager()->persist($subGroup);
+        $test = new DigitalLineTest();
+        $test->setName('adasdasd');
+        $test->setDigitalLineTestSubGroup($subGroup);
+        $this->getDoctrine()->getManager()->persist($test);
+        var_dump(count($subGroup->getDigitalLineTests()));
         return $this->render('test/index.html.twig', [
             'controller_name' => 'TestController',
         ]);
