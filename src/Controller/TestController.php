7d3999258b2a1456336ce5acf5b79e1c62cac31e
Sergey Krivobokov <krivobokov.sv@academia-moscow.ru>
2021-09-05 06:27:11 +0300

    digital subgroup fix

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -7,6 +7,7 @@ use App\Repository\DigitalLineTestRepository;
 use App\Repository\DigitalLineTestSubGroupRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class TestController extends AbstractController
 {
