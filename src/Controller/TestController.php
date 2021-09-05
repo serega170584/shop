d77e0e7aa5d636bd03849f4a32872c882c01718d
Sergey Krivobokov <krivobokov.sv@academia-moscow.ru>
2021-09-05 06:26:35 +0300

    digital subgroup fix

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -4,21 +4,22 @@ namespace App\Controller;
 
 use App\Repository\DigitalLineTestGroupRepository;
 use App\Repository\DigitalLineTestRepository;
+use App\Repository\DigitalLineTestSubGroupRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class TestController extends AbstractController
 {
     /**
      * @Route("/test", name="test")
+     * @param DigitalLineTestSubGroupRepository $digitalLineTestSubGroupRepository
      * @param DigitalLineTestRepository $digitalLineTestRepository
      * @param DigitalLineTestGroupRepository $digitalLineTestGroupRepository
      * @return Response
      */
-    public function index(DigitalLineTestRepository $digitalLineTestRepository, DigitalLineTestGroupRepository $digitalLineTestGroupRepository): Response
+    public function index(DigitalLineTestSubGroupRepository $digitalLineTestSubGroupRepository, DigitalLineTestRepository $digitalLineTestRepository, DigitalLineTestGroupRepository $digitalLineTestGroupRepository): Response
     {
-        $tests = $digitalLineTestRepository->findAll();
+        $tests = $digitalLineTestSubGroupRepository->findAll();
         return $this->render('test/index.html.twig', [
             'controller_name' => 'TestController',
         ]);
