8f532afbeaf998ffdef3da51583d51879f6e9e9d
Sergey Krivobokov <krivobokov.sv@academia-moscow.ru>
2021-09-15 12:45:35 +0300

    category manager fix

--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -4,6 +4,8 @@ namespace App\Repository;
 
 use App\Entity\Product;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Criteria;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -32,5 +34,17 @@ class ProductRepository extends ServiceEntityRepository
             ->getQuery()
             ->getResult();
     }
-}
 
+    /**
+     * @return Product[]|ArrayCollection
+     */
+    public function findPopular()
+    {
+        return new ArrayCollection($this->findBy([
+            self::IS_POPULAR => true
+        ], [
+            self::ID => Criteria::DESC
+        ], self::POPULAR_LIMIT
+        ));
+    }
+}
