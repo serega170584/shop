96c299848fa4172f86f9c2f1579102de8325a8ea
Sergey Krivobokov <krivobokov.sv@academia-moscow.ru>
2021-09-18 12:00:11 +0300

    main page basket refactoring

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -20,6 +20,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
