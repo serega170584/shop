8f1d25fbdb52517e182205929775c86bf0ab3a78
Sergey Krivobokov <krivobokov.sv@academia-moscow.ru>
2021-09-21 09:37:26 +0300

    main page basket refactoring

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -23,7 +23,7 @@ class ProductController extends AbstractController
     }
 
     /**
-     * @Route("/test/product/test/{slug}/buy", name="buy")
+     * @Route("/test/test/{slug}/product/buy", name="buy")
      * @param Product $product
      * @return Response
      */
