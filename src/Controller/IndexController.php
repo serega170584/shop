6de072f471286012af75d06eb4be4582c9b85ea0
Sergey Krivobokov <krivobokov.sv@academia-moscow.ru>
2021-09-27 11:53:44 +0300

    controller fix

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -18,6 +18,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
